1f4876fe53f543ed3a5459f3848a75d88d2201fe
liuting <liuting@91fd.tech>
2020-04-20 10:39:03 +0800

    .

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,33 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="test">
+    <h1>test count: {{ count }}</h1>
+    <div>count * 2 = {{ doubleCount }}</div>
+    <button @click="add">add</button>
   </div>
 </template>
+
+<script>
+import { ref, computed, watch } from "vue";
+
+export default {
+  setup() {
+    const count = ref(0);
+    const add = () => {
+      count.value++;
+    };
+    watch(
+      () => count.value,
+      val => {
+        console.log(`count is ${val}`);
+      }
+    );
+    const doubleCount = computed(() => count.value * 2);
+
+    return {
+      count,
+      doubleCount,
+      add
+    };
+  }
+};
+</script>
